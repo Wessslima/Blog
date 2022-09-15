65d4797798aa1df846823a198ddb94013536c0fd
unknown <wes_lima@outlook.com>
2022-09-15 11:30:44 -0300

    Adicionado a camada Service

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -38,7 +38,7 @@ public class Usuario {
 	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
 
-	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
